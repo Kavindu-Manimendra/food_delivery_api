a82f003011b0abb63bcd3f2962b296b9fbab8efe
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2026-01-05 15:19:16 +0530

    food apis added

---
+++ src/main/java/org/foodapi/controller/FoodController.java
@@ -0,0 +1,50 @@
+package org.foodapi.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.foodapi.dtos.FoodRequest;
+import org.foodapi.dtos.FoodResponse;
+import org.foodapi.service.FoodService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/foods")
+@RequiredArgsConstructor
+public class FoodController {
+
+    private final FoodService foodService;
+
+    @PostMapping
+    public FoodResponse addFood(@RequestPart("food") String foodString, @RequestPart("file") MultipartFile file) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        FoodRequest request = null;
+        try {
+            request = objectMapper.readValue(foodString, FoodRequest.class);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid JSON format.");
+        }
+        FoodResponse foodResponse = foodService.addFood(request,file);
+        return foodResponse;
+    }
+
+    @GetMapping
+    public List<FoodResponse> readFoods() {
+        return foodService.readFoods();
+    }
+
+    @GetMapping("/{id}")
+    public FoodResponse readFood(@PathVariable String id) {
+        return foodService.readFood(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFood(@PathVariable String id) {
+        foodService.deleteFood(id);
+    }
+}
