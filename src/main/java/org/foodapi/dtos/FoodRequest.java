969a2784c4a6cfde80190bc66028d2c58a2e25d8
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2026-01-05 15:18:54 +0530

    food entity and repos added

---
+++ src/main/java/org/foodapi/dtos/FoodRequest.java
@@ -0,0 +1,15 @@
+package org.foodapi.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FoodRequest {
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+}
