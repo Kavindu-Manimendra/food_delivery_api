70cc0404b958011cfcdd39720fe0777abfee3773
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2026-04-03 12:20:09 +0530

    auth apis added

---
+++ src/main/java/org/foodapi/dtos/AuthenticationResponse.java
@@ -0,0 +1,11 @@
+package org.foodapi.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String email;
+    private String token;
+}
