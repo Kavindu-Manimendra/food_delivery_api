24021b6d170d3261587ad4b49d5421545da86e05
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2026-04-03 12:19:45 +0530

    security config added

---
+++ src/main/java/org/foodapi/service/impl/AppUserDetailsService.java
@@ -0,0 +1,27 @@
+package org.foodapi.service.impl;
+
+import lombok.AllArgsConstructor;
+import org.foodapi.entity.UserEntity;
+import org.foodapi.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+@AllArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new User(user.getEmail(), user.getPassword(), Collections.emptyList());
+    }
+}
