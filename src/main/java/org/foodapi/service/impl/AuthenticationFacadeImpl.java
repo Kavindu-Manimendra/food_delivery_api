465655e8ad8c2551f108894d7a218afd6a1a8d70
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2026-04-06 10:09:52 +0530

    authFacade added

---
+++ src/main/java/org/foodapi/service/impl/AuthenticationFacadeImpl.java
@@ -0,0 +1,14 @@
+package org.foodapi.service.impl;
+
+import org.foodapi.service.AuthenticationFacade;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacadeImpl implements AuthenticationFacade {
+    @Override
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+}
