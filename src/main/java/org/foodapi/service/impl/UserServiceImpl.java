dec852452df99b3a3c87b2b084695d5a5006ddc1
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2026-04-06 10:10:19 +0530

    user service func added

--- src/main/java/org/foodapi/service/impl/UserServiceImpl.java
+++ src/main/java/org/foodapi/service/impl/UserServiceImpl.java
@@ -5,7 +5,9 @@ import org.foodapi.dtos.UserRequest;
 import org.foodapi.dtos.UserResponse;
 import org.foodapi.entity.UserEntity;
 import org.foodapi.repository.UserRepository;
+import org.foodapi.service.AuthenticationFacade;
 import org.foodapi.service.UserService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,21 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationFacade authenticationFacade;
 
     @Override
     public UserResponse registerUser(UserRequest request) {
         UserEntity newUser = convertToEntity(request);
         newUser = userRepository.save(newUser);
         return convertToResponse(newUser);
+    }
+
+    @Override
+    public String findByUserId() {
+        String loggedInUserEmail = authenticationFacade.getAuthentication().getName();
+        UserEntity loggedInUser = userRepository.findByEmail(loggedInUserEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return loggedInUser.getId();
     }
 
     private UserEntity convertToEntity(UserRequest request) {
