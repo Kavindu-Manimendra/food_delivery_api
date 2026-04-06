382495636d1e5a8837dcb185ba337c6943f8e60d
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2026-04-06 10:14:00 +0530

    cart apis added

---
+++ src/main/java/org/foodapi/controller/CartController.java
@@ -0,0 +1,46 @@
+package org.foodapi.controller;
+
+import lombok.AllArgsConstructor;
+import org.foodapi.dtos.CartRequest;
+import org.foodapi.dtos.CartResponse;
+import org.foodapi.service.CartService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/cart")
+@AllArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping
+    public CartResponse addToCart(@RequestBody CartRequest request) {
+        String foodId = request.getFoodId();
+        if (foodId == null || foodId.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Food Id not found.");
+        }
+        return cartService.addToCart(request);
+    }
+
+    @GetMapping
+    public CartResponse getCart() {
+        return cartService.getCart();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void clearCart() {
+        cartService.clearCart();
+    }
+
+    @PostMapping("/remove")
+    public CartResponse removeFromCart(@RequestBody CartRequest request) {
+        String foodId = request.getFoodId();
+        if (foodId == null || foodId.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Food Id not found.");
+        }
+        return cartService.removeFromCart(request);
+    }
+}
