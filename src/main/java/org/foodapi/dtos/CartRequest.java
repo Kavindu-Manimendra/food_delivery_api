4cfd18bb43843408f2841a6f1fe65c276ef9e888
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2026-04-06 10:13:41 +0530

    cart dtos added

---
+++ src/main/java/org/foodapi/dtos/CartRequest.java
@@ -0,0 +1,14 @@
+package org.foodapi.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartRequest {
+    private String foodId;
+}
